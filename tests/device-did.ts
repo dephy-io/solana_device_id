2fbd7b4cd6f60929d2dd955c2e74ef2f6a25d391
liushooter <shooter.hero@gmail.com>
2024-03-01 00:58:25 +0800

    update test case

--- tests/device-did.ts
+++ tests/device-did.ts
@@ -1,16 +1,52 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { DeviceDid } from "../target/types/device_did";
+
+// export ANCHOR_PROVIDER_URL="https://api.devnet.solana.com"
+// export ANCHOR_WALLET=~/.config/solana/id.json
 
 describe("device-did", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const conn = new Connection("http://0.0.0.0:8899")
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.DeviceDid as Program<DeviceDid>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it.skip("Is initialized!", async () => {
   });
+
+  it("Initialize Admin", async () => {
+    const signer = provider.wallet;
+
+    const admin = anchor.web3.Keypair.generate();
+    const admin_pk = admin.publicKey;
+
+    const authority = anchor.web3.Keypair.generate();
+    const authority_pk = authority.publicKey;
+
+    const treasury = anchor.web3.Keypair.generate();
+    const treasury_pk = treasury.publicKey;
+
+    interface InitializeAdminArgs {
+      admin: PublicKey;
+      authority: PublicKey;
+      treasury: PublicKey;
+    }
+
+    let args: InitializeAdminArgs = {
+      admin: admin_pk,
+      authority: authority_pk,
+      treasury: treasury_pk
+    }
+
+    const tx = await program.methods.initializeAdmin(args)
+      .accounts({
+        payer: signer.publicKey,
+      })
+      .rpc();
+  });
+
 });
